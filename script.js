69ea7ebb076e90c0876ae341ac1c7be575d963d8
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-10-28 21:58:57 +0700

    feat: make the screen into two parts

--- script.js
+++ script.js
@@ -1,12 +1,75 @@
-function operate(firstNumber, operator, secondNumber) {
-    if (operator === "*") {
-        return firstNumber * secondNumber;
-    } else if (operator === "/") {
-        return firstNumber / secondNumber;
-    } else if (operator === "+") {
-        return firstNumber + secondNumber;
-    } else if (operator === "-") {
-        return firstNumber - secondNumber;
+const screenDisplay = document.querySelector(".screen");
+const lastScreen = document.querySelector(".last-screen");
+const currentScreen = document.querySelector(".current-screen");
+const buttonNumber = document.querySelectorAll(".button-number");
+const buttonOperator = document.querySelectorAll("#button-operator");
+const buttonEquals = document.querySelector(".button-equals");
+
+let firstNumber = "";
+let operator = null;
+let secondNumber = "";
+
+buttonNumber.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        enterNumberToScreen(elem.textContent);
+    });
+});
+
+function enterNumberToScreen(number){
+    currentScreen.textContent += number
+};
+
+function setOperate(){
+    firstNumber = currentScreen.textContent;
+}
+
+buttonOperator.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        operator = elem.textContent;
+        displayValue = operator;
+        screenDisplay.textContent = `${firstNumber} ${operator}`;
+        return opClicked = true;
+    })
+})
+
+buttonEquals.addEventListener("click", () => {
+    console.log("button equals")
+})
+
+function add(a, b) {
+    return a + b;
+};
+function subtract(a, b) {
+    return a - b;
+};
+function multiply(a, b) {
+    return a * b;
+};
+function divide(a, b) {
+    return a / b;
+};
+
+
+function operate(operator, firstNumber, secondNumber) {
+    let firstNumbers = Number(firstNumber);
+    let secondNumbers = Number(secondNumber);
+    switch (operator) {
+        case "+":
+            add(firstNumbers, secondNumbers);
+            break;
+        case "-":
+            subtract(firstNumbers, secondNumbers);
+            break;
+        case "*":
+            multiply(firstNumbers, secondNumbers);
+            break;
+        case "/":
+            divide(firstNumbers, secondNumbers);
+            break;
     };
 };
-console.log(operate(1, '-', 12));
+console.log(operate('+', 1, 2));
+
+// nomor yang diinput dengan button harus disimpan di firstNumber dan secondNumber
+// setehal itu nilai keduanya disimpan ke displayValue untuk ditampilkan
+// jika button operator ditekan maka value disimpan di secondNumber
