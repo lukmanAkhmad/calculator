60fda417b0e1f2691a2789e394fd3937498349e2
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-11-06 16:47:56 +0700

    feat: add backspace button

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const buttonNumber = document.querySelectorAll(".button-number");
 const buttonOperator = document.querySelectorAll("#button-operator");
 const buttonEquals = document.querySelector(".button-equals");
 const clearButton = document.querySelector(".button-clear");
+const buttonBackSpace = document.querySelector(".button-backspace");
 
 buttonEquals.addEventListener("click", evaluate);
 clearButton.addEventListener("click", clearData);
+buttonBackSpace.addEventListener("click", backSpace);
 
 let firstNumber = "";
 let currentOperator = null;
@@ -17,18 +19,21 @@ buttonNumber.forEach((elem) => {
     elem.addEventListener("click", () => {
         if (currentOperator !== null) {
             secondNumber += elem.textContent;
-            enterNumberToScreen(secondNumber)
-            console.log(`Second Number = ${secondNumber}`)
+            enterNumberToScreen(secondNumber);
+            console.log(`Second Number = ${secondNumber}`);
         } else if (currentOperator === null) {
             firstNumber += elem.textContent;
-            enterNumberToScreen(firstNumber)
-            console.log(`First Number = ${firstNumber}`)
-        }
+            enterNumberToScreen(firstNumber);
+            console.log(`First Number = ${firstNumber}`);
+        };
     });
 });
 
 buttonOperator.forEach((elem) => {
     elem.addEventListener("click", () => {
+        if(currentOperator !== null){
+            evaluate();
+        };
         setOperation(elem.textContent);
     });
 });
@@ -38,38 +43,53 @@ function enterNumberToScreen(number) {
 };
 
 function setOperation(operator) {
-    currentOperator = operator;
     if (currentOperator !== null) {
         firstNumber = currentScreen.textContent;
         secondNumber = "";
     };
+    currentOperator = operator;
     lastScreen.textContent = `${firstNumber} ${currentOperator}`;
-
 };
 
 function evaluate() {
-    if(currentOperator === null) return;
+    if (currentOperator === null) return;
+    if (currentOperator === "/" && (firstNumber === "0" || secondNumber === "0")) {
+        secondNumber = "";
+        return currentScreen.textContent = "You cannot divide by zero !!!";
+    };
     currentScreen.textContent = convertToDecimal(
         operate(currentOperator, firstNumber, secondNumber)
     );
     lastScreen.textContent = `${firstNumber} ${currentOperator} ${secondNumber}`;
 };
 
-function clearData(){
+function clearData() {
     lastScreen.textContent = "";
     currentScreen.textContent = "";
     firstNumber = "";
     currentOperator = null;
     secondNumber = "";
-}
+};
+
+function backSpace() {
+    if (currentOperator === null) {
+        let getNumber = currentScreen.textContent;
+        let deleteNumber = getNumber.slice(0, -1);
+        currentScreen.textContent = deleteNumber;
+        firstNumber = currentScreen.textContent;
+        console.log(firstNumber)
+    } else if (currentOperator !== null) {
+        let getNumber = currentScreen.textContent;
+        let deleteNumber = getNumber.slice(0, -1);
+        currentScreen.textContent = deleteNumber;
+        secondNumber = currentScreen.textContent;
+        console.log(secondNumber)
+    }
+};
 
 function convertToDecimal(number) {
     return Math.round(number * 1000) / 1000;
-}
-
-buttonEquals.addEventListener("click", () => {
-    console.log("button equals")
-})
+};
 
 function add(a, b) {
     return a + b;
